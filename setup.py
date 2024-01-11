eb2e5025dcf4cb506922ae5564c45538bd6995db
Ashutosh10812 <137986226+Ashutosh10812@users.noreply.github.com>
2024-01-11 20:42:45 +0530

    setup file added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'Ashutosh Sharma',
+    author_email= 'ashstargazer21@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)

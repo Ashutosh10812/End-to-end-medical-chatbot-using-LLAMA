28b9c33a63a7738f0d11e4dccf896010d69231cf
Ashutosh10812 <137986226+Ashutosh10812@users.noreply.github.com>
2024-01-11 22:32:25 +0530

    templates added

---
+++ src/prompt.py
@@ -0,0 +1,10 @@
+prompt_template=""""
+Use the following piece of information to answer the user's question.
+If you dont know the answer,just say that you dont know,dont try to make up an answer.
+
+Context:{context}
+Question={question}
+
+Only return the helpful answer below and nothing else.
+Helpful answer:
+"""
